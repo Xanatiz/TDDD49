ada86df71dfa7535a7670ae2ed37167bba2c1636
Xanatiz <henrik.thoreson@gmail.com>
2013-12-13 14:55:13 +0100

    huh

--- chess106/Unit.cs
+++ chess106/Unit.cs
@@ -10,18 +10,7 @@ namespace chess106
 {
     class Unit 
     {
-        private Rules rules = new Rules();
-       /* private chessPieces[,] chessboardArray =
-        {{chessPieces.BlackRook, chessPieces.BlackKnight, chessPieces.BlackBishop, chessPieces.BlackQueen, chessPieces.BlackKing, chessPieces.BlackBishop, chessPieces.BlackKnight, chessPieces.BlackRook},
-        {chessPieces.BlackPawn, chessPieces.BlackPawn, chessPieces.BlackPawn, chessPieces.BlackPawn, chessPieces.BlackPawn, chessPieces.BlackPawn, chessPieces.BlackPawn, chessPieces.BlackPawn},
-        {chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None},
-        {chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None},
-        {chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None},
-        {chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None,chessPieces.None},
-        {chessPieces.WhitePawn, chessPieces.WhitePawn, chessPieces.WhitePawn, chessPieces.WhitePawn, chessPieces.WhitePawn, chessPieces.WhitePawn, chessPieces.WhitePawn, chessPieces.WhitePawn},
-        {chessPieces.WhiteRook, chessPieces.WhiteKnight, chessPieces.WhiteBishop, chessPieces.WhiteQueen, chessPieces.WhiteKing, chessPieces.WhiteBishop, chessPieces.WhiteKnight, chessPieces.WhiteRook}};
-        */
-        private Pieces[,] chessboardArray = { { new Rook(0, 0, Team.BLACK), new Knight(0, 1, Team.BLACK), new Bishop(0, 2, Team.BLACK), new Queen(0, 3, Team.BLACK), new King(0, 4, Team.BLACK), new Bishop(0, 5, Team.BLACK), new Knight(0, 6, Team.BLACK), new Rook(0, 7, Team.BLACK)},
+        static Pieces[,] chessboardArray = { { new Rook(0, 0, Team.BLACK), new Knight(0, 1, Team.BLACK), new Bishop(0, 2, Team.BLACK), new Queen(0, 3, Team.BLACK), new King(0, 4, Team.BLACK), new Bishop(0, 5, Team.BLACK), new Knight(0, 6, Team.BLACK), new Rook(0, 7, Team.BLACK)},
                                               { new Pawn(1, 0, Team.BLACK), new Pawn(1, 1, Team.BLACK), new Pawn(1, 2, Team.BLACK), new Pawn(1, 3, Team.BLACK), new Pawn(1, 4, Team.BLACK), new Pawn(1, 5, Team.BLACK), new Pawn(1, 6, Team.BLACK), new Pawn(1, 7, Team.BLACK) },
                                               { new Pieces(2,0, Team.NONE), new Pieces(2,1, Team.NONE), new Pieces(2,2, Team.NONE), new Pieces(2,3, Team.NONE), new Pieces(2,4, Team.NONE), new Pieces(2,5, Team.NONE), new Pieces(2,6, Team.NONE), new Pieces(2,7, Team.NONE)},
                                               { new Pieces(3,0, Team.NONE), new Pieces(3,1, Team.NONE), new Pieces(3,2, Team.NONE), new Pieces(3,3, Team.NONE), new Pieces(3,4, Team.NONE), new Pieces(3,5, Team.NONE), new Pieces(3,6, Team.NONE), new Pieces(3,7, Team.NONE)},
@@ -32,9 +21,12 @@ namespace chess106
 
         public void move(int fromColumn, int fromRow, int toColumn, int toRow)
         {
+            Pieces temp;
             if(chessboardArray[fromRow, fromColumn].isMovePossible(toColumn, toRow))
-            { //utför förflyttning;
-            
+            {
+                temp = chessboardArray[toRow, toColumn];
+                chessboardArray[toRow, toColumn] = chessboardArray[fromRow, fromColumn];
+                chessboardArray[fromRow, fromColumn] = temp;
             }
         }
 
